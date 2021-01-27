7e672d9361ebc9016a4d1b08fad4222af61a132d
w.hunt <wfh2d88@gmail.com>
2021-01-27 09:02:33 -0600

    functioning minus saved button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,111 @@
-var savedDiv = document.querySelector(".savedDiv");
-var rightWrap = document.querySelector(".rightWrap")
-var searchBtn = document.querySelector(".searchBtn");
+// Add Img Alts
+// Add Notes
+// Refactor so that city from saved button can be passed
+//
+
+
+
 var city = $("#cityInput").val().trim();
 var key = "f183369b23d768246e94cc35ace9f5f9"
 var current = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
 
 
 //Button is clicked and value is taken
-$(searchBtn).click(function(){
+$(".searchBtn").click(function() {
     // Gets value of input
     var city = $("#cityInput").val().trim();
+
     // add name to list in leftSide
-    var savedCity = document.createElement("button")
-    savedCity.innerHTML = city;
-    savedDiv.appendChild(savedCity);
+    $(".savedDiv").append("<button class ='savedBtn'>" + city + "</button>");
 
     // add name of city to right side
-    rightWrap.innerHTML = " ";
-    var cityDisplay = document.createElement("span");
-    cityDisplay.innerHTML = city;
-    rightWrap.appendChild(cityDisplay);
+    $(".rightWrap").html(" ");
+    $(".rightWrap").append("<span class='card-title'>" + city + "</span>")
 
-    // add date to right side
+    // get todays date for dipslay
     var date = moment().format("dddd, MMMM Do YYYY");
-    console.log(date);
-    var dateDisplay=document.createElement("span");
-    dateDisplay.innerHTML = date;
-    rightWrap.appendChild(dateDisplay);
+     
+    // add date to right side
+    $(".rightWrap").append("<span class = card-subtile>" + date + "</span>")
 
-
-    // assigns url to a variable 
+    // assigns current api url to a variable 
     var current = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key;
     
     // fetches data package
     fetch(current).then(function(response){
         if(response.ok) {
             response.json().then(function(data){
-                createCurrent(data);
+                futureFetch(data);
             })
         } else {
             // alert to please choose valid city
         }
-    })
+    });
 });
 
-
-var createCurrent = function (data) {
-    // temp, humidity, wind speed, icon rep' of weather, uv index.
-
-    // UV index remains
-    var wind = data.wind.speed
-    console.log(wind);
-
-    var humidity = data.main.humidity;
-    console.log(humidity);
-
-    var description = data.weather[0].description;
-    console.log(description);
-
+var futureFetch = function(data){
     var lat = data.coord.lat;
     var lon = data.coord.lon;
 
-    // weather.icon - asuccesffuly retrieved and appened to target div
-    var icon = data.weather[0].icon;
-    console.log(icon);
-    var iconUrl = "https://openweathermap.org/img/wn/" + icon + ".png";
-    var iconImg = document.createElement("img");
-    iconImg.setAttribute("src", iconUrl);
-    iconImg.setAttribute("height", "80px")
-    rightWrap.append(iconImg);
-
-    var fiveDay = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + key;
+    var fiveDay = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude={part}&appid=" + key;
     fetch(fiveDay).then(function(response){
-        if(response.ok) {
+        if(response.ok){
             response.json().then(function(data){
-                createFuture(data);
+                createHTML(data);
             })
         }
     });
-}
+};
 
-var createFuture = function(data) { 
-    // still need date display
+var createHTML = function (data) {
+    // data variables 
+    var wind = data.current.wind_speed;
+    var humidity = data.current.humidity;
+    var description = data.current.weather[0].description;
+    var uvI = data.current.uvi;
+    var farenheit = Math.floor(data.current.temp);
+    var celsius = Math.floor(farenheit - 32);
+    var icon = data.current.weather[0].icon;
 
-    // variables for data
-    var uvi = data.current.uvi;
-    var dailyHum = data.daily[0].humidity;// for loop
-    var dailyHi = data.daily[0].temp.max;
-    var dailyLow = data.daily[0].temp.min;
+    // clear conditions div and appends new conditions
+    $(".conditions").html(" ");
+    $(".conditions").append("<li> Wind Speed: " + wind + " mph </li><li> Humidity: " + humidity + " % </li><li> UV Index: " + uvI + "</li><li>" + description + "</li>");
 
-    // send UV index to be applied to HTML
-    createUvi(uvi);
+    // Current Temperature displayed as card body 
+    $(".rightWrap").append("<h2 class='card-body'>" + farenheit + " F|| " + celsius + " C")
+
+    // weather.icon retrieved and appened to target div
+    var iconUrl = "https://openweathermap.org/img/wn/" + icon + ".png";
+    $(".rightWrap").prepend("<img class='card-img-top' src =" + iconUrl + " height=120px>")
 
     // create cards for five day forecast
+    today = moment();
+    var tomorrow = today.add(1, 'days').format("L");
+    var day2 = today.add(1, "days").format("L");
+    var day3 = today.add(1, "days").format("L");
+    var day4 = today.add(1, "days").format("L");
+    var day5 = today.add(1, "days").format("L");
 
+    var dayDivs = [".dayPlusOne", ".dayPlusTwo", ".dayPlusThree", ".dayPlusFour", ".dayPlusFive"]
+    var days = [tomorrow , day2 , day3, day4, day5]
+
+    for(var i = 0; i < 5; i++){
+        var dailyHum = data.daily[i].humidity;// for loop
+        var dailyHi = Math.floor(data.daily[i].temp.max);
+        var dailyLow = Math.floor(data.daily[i].temp.min);
+        var fiveIcon = data.daily[i].weather[0].icon;
+        var fiveIconUrl = "https://openweathermap.org/img/wn/" + fiveIcon + ".png";
+        $(dayDivs[i]).html(" ");
+
+        $(dayDivs[i]).append("<h4 class='card-title'>" + days[i] + " </h4>" + "<img height=.5em class='card-img-top' src=" + fiveIconUrl + "></img><h5>" + dailyHi + "F - Hi </h5><h5>" + dailyLow + "F - Low </h5><h5> Humidity: " + dailyHum + "% </h5>");
+
+    }
 };
 
-var createUvi = function(uvi) {
-    //create or request colored icon to represent UV index
-    console.log(uvi);
-};
+
+
+
+
 
 
 
